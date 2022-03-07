f38117c7c69eb7b965401f3c900ddbf46c46cc7f
Yuta Fukagawa <25516089+yfskyline@users.noreply.github.com>
2022-03-08 05:30:12 +0900

    Improve: improve test-data

--- mysql-test/initdb/3_insert_tests_table.sql
+++ mysql-test/initdb/3_insert_tests_table.sql
@@ -1,8 +1,8 @@
-INSERT INTO `rtt_db`.`prefix_sid_rtt` (`id`, `src`, `dest_prefix`, `sid`, `rtt`) VALUES ('1', 'fd00::1', '2001:db8::/64', '2001:200:e00:e4e::1', '4');
-INSERT INTO `rtt_db`.`prefix_sid_rtt` (`id`, `src`, `dest_prefix`, `sid`, `rtt`) VALUES ('2', 'fd00::1', '2001:db8::/64', '2001:200:e00:e4e::2', '8');
-INSERT INTO `rtt_db`.`prefix_sid_rtt` (`id`, `src`, `dest_prefix`, `sid`, `rtt`) VALUES ('3', '2001:db8:0:4::2', '::/0', '2001:200:1111:ffff::2', '140');
-INSERT INTO `rtt_db`.`prefix_sid_rtt` (`id`, `src`, `dest_prefix`, `sid`, `rtt`) VALUES ('4', '2001:db8:0:1::1', '::/0', NULL, '159');
-INSERT INTO `rtt_db`.`prefix_sid_rtt` (`id`, `src`, `dest_prefix`, `sid`, `rtt`) VALUES ('5', '2001:db8:0:4::2', '::/0', '2001:db8:1111:ffff::2', '204');
-INSERT INTO `rtt_db`.`prefix_sid_rtt` (`id`, `src`, `dest_prefix`, `sid`, `rtt`) VALUES ('6', '2001:db8:0:4::2', '::/0', '2001:db8:1111:ffff::2', '139');
-INSERT INTO `rtt_db`.`prefix_sid_rtt` (`id`, `src`, `dest_prefix`, `sid`, `rtt`) VALUES ('7', '2001:db8:0:4::2', '::/0', '2001:db8:1111:ffff::2', '151');
-INSERT INTO `rtt_db`.`prefix_sid_rtt` (`id`, `src`, `dest_prefix`, `sid`, `rtt`) VALUES ('8', '2001:db8:0:4::2', '::/0', '2001:db8:1111:ffff::2', '164');
+INSERT INTO `rtt_db`.`prefix_sid_rtt` (`id`, `src`, `dest_prefix`, `sid`, `rtt`) VALUES ('1', '2001:db8:0:4::2', '2001:db8:1234::/64', '2001:200:1111:ffff::1', '13');
+INSERT INTO `rtt_db`.`prefix_sid_rtt` (`id`, `src`, `dest_prefix`, `sid`, `rtt`) VALUES ('2', '2001:db8:0:4::2', '2001:db8:1234::/64', '2001:200:1111:ffff::2', '8');
+INSERT INTO `rtt_db`.`prefix_sid_rtt` (`id`, `src`, `dest_prefix`, `sid`, `rtt`) VALUES ('3', '2001:db8:0:4::2', '2001:db8:1234::/64', '2001:200:1111:ffff::2', '140');
+INSERT INTO `rtt_db`.`prefix_sid_rtt` (`id`, `src`, `dest_prefix`, `sid`, `rtt`) VALUES ('4', '2001:db8:0:4::2', '2001:db8:4321::/64', NULL, '159');
+INSERT INTO `rtt_db`.`prefix_sid_rtt` (`id`, `src`, `dest_prefix`, `sid`, `rtt`) VALUES ('5', '2001:db8:0:4::2', '2001:db8:4321::/64', '2001:db8:1111:ffff::1', '204');
+INSERT INTO `rtt_db`.`prefix_sid_rtt` (`id`, `src`, `dest_prefix`, `sid`, `rtt`) VALUES ('6', '2001:db8:0:4::2', '2001:db8:4321::/64', '2001:db8:1111:ffff::2', '139');
+INSERT INTO `rtt_db`.`prefix_sid_rtt` (`id`, `src`, `dest_prefix`, `sid`, `rtt`) VALUES ('7', '2001:db8:0:4::2', '2001:db8:4321::/64', '2001:db8:1111:ffff::3', '151');
+INSERT INTO `rtt_db`.`prefix_sid_rtt` (`id`, `src`, `dest_prefix`, `sid`, `rtt`) VALUES ('8', '2001:db8:0:4::2', '2001:db8:4321::/64', '2001:db8:1111:ffff::4', '164');
