78898e28c19e02a750e694f7ed05237216973668
Yuta Fukagawa <25516089+yfskyline@users.noreply.github.com>
2022-03-08 06:47:04 +0900

    Rename: dest_prefix to dst_prefix

--- mysql-test/initdb/2_create_tests_table.sql
+++ mysql-test/initdb/2_create_tests_table.sql
@@ -1,7 +1,7 @@
 CREATE TABLE IF NOT EXISTS `rtt_db`.`prefix_sid_rtt` (
   `id` int(10) unsigned NOT NULL AUTO_INCREMENT,
   `src` varchar(39),
-  `dest_prefix` varchar(43),
+  `dst_prefix` varchar(43),
   `sid` varchar(39),
   `rtt` int(32) unsigned,
   PRIMARY KEY (`id`)
