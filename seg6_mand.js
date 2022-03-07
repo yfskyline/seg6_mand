e29894d022956171b71f6ef50ae75a5511c2f3dc
Yuta Fukagawa <25516089+yfskyline@users.noreply.github.com>
2022-03-07 22:55:08 +0900

    Improve: process each row

--- seg6_mand.js
+++ seg6_mand.js
@@ -3,6 +3,8 @@ const commandLineUsage = require('command-line-usage');
 const { execSync } = require('child_process');
 const fs = require('fs');
 const mysql = require('mysql');
+const { Etcd3 } = require('etcd3');
+const client = new Etcd3();
 
 const optionDefinitions = [
         {
@@ -67,12 +69,13 @@ if (options.debug) { console.log('lastId: ' + lastId); }
 // Current Routes
 const stdout = execSync('ip -6 route show');
 if (options.debug) { console.log("*******CURRENT ROUTE********"); }
-if (options.debug) {console.log(`$ ip -6 route show: \n${stdout.toString()}`);}
+if (options.debug) { console.log(`$ ip -6 route show: \n${stdout.toString()}`); }
 
 
 // Get new prefix/sid/rtt from rtt_db
 // Connect to MySQL
 const connection = mysql.createConnection({
+
 	host: 'localhost',
 	port: 13306,
 	user: 'root',
@@ -82,14 +85,33 @@ const connection = mysql.createConnection({
 });
 
 connection.connect();
-connection.query('SELECT * FROM `prefix_sid_rtt` WHERE id > ' + lastId, (error, results, fields) => {
+// get all rows with ID greater than lastID
+connection.query('SELECT * FROM `rtt_db`.`prefix_sid_rtt` WHERE id > ' + lastId, (error, results, fields) => {
 	if (error) throw error;
-	console.log(results[0]);
-	console.log(results[0].id);
-	console.log(results[0].dest_prefix);
-	console.log(results[0].sid);
-	console.log(results[0].rtt);
-	console.log('results.length' + results[results.length-1].id);
+	results.forEach(function(result) {
+		if (options.debug) {
+			console.log(result);
+			console.log('result.id: ' + result.id);
+			console.log('results.dst_prefix: ' + result.dest_prefix);
+			console.log('results.sid: ' + result.sid);
+			console.log('results.rtt: ' + result.rtt);
+		}
+
+		// sidがNULLかどうか
+		if (result.sid === null) {
+			console.log('NULL!!!');
+			// etcdから該当prefixの一覧を取得する
+			// getSid(prefix)
+			// デフォルトSIDを優先して(もしくは適当にどちらかを優先して)経路を埋め込む
+			// addRoute(prefix, sids, preferSid);
+		} else {
+			// rtt_dbから同じprefix/sidを持つrowの中からidが最大のものをそれぞれ取得
+			// 全てのprefix/sidの中でRTTが最小のsidを取得
+			// addRoute(prefix, sids, preferSid);
+			console.log('NOT NULL SID!');
+		}
+	});
+	console.log('=======Processed ' + results[results.length-1].id + ' Prefix========');
 	// write the current id to the file
 	fs.writeFileSync("lastId.txt", results[results.length-1].id.toString(), 'utf-8', (err) =>{
 		if (err) {
@@ -100,32 +122,26 @@ connection.query('SELECT * FROM `prefix_sid_rtt` WHERE id > ' + lastId, (error, 
 });
 connection.end();
 
-// id以上のrowを全て取得
-
-
-// 取得した経路を1つ1つループ処理
-	// sidがNULLかどうかで場合分け
-	// NULLだった場合
-		// /etcdから該当prefixのsid一覧を取得する
-			// getSid(prefix)
-			// デフォルトSIDを優先して，経路を埋め込む
-		// sid一覧を取得する
-		// 
-	// sidがある場合
-		// rtt_dbから同じprefixを持つrowをsidが最大のものをそれぞれ取得してくる
-		// RTTが最小のsidを取得
-		// 経路のベストSIDを指定してprefixのFIBへの埋め込み
-
-
-
-
 
 // 複数のprefix_sidをipコマンドで埋め込む関数
+function addRoute (prefix, sids, preferSid) {
+	console.log('ADDROUTE');
+	console.log(prefix);
+	console.log(sids);
+	console.log(preferSid);
+}
+let command = 'ip -6 route add';
+let prefix = '::/0';
+let sid = '2001:db8:1111:ffff::2';
+let sids = ['0', '1'];
+addRoute(prefix, sids, sid);
+let commandResult = execSync('ip -6 route show');
+if (options.debug) { console.log(`$ ip -6 route show: \n${stdout.toString()}`); }
 // 既存の経路があったときにうまく処理する
 // prefixとsidリストとコストを引数で取る
 
 // 既存の経路のsidの優先順位が逆転した場合にイプシロングリーディのベストSIDを入れ替える関数
-
+console.log(options.epsilon);
 // prefixを引数にしてetcdから該当するprefix_sid_listを取得する関数
 
 if (options.debug) { console.log("*******DEBUG MODE********"); }
